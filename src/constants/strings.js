2095941899ef9a2e732fe13760f7152af5d33723
Lê Văn Nhật <nhat.le2@mservice.com.vn>
2022-05-01 14:41:23 +0700

    refactor: clean code and add string file

---
+++ src/constants/strings.js
@@ -0,0 +1,18 @@
+const strings = {
+    // navbar 
+    topGame :'Top Game',
+    newGame : 'New Games',
+    slots :'Slots',
+    jackpots :'Jackpots',
+    blackjack:'Blackjack',
+    live:'Live',
+    roulette:'Roulette',
+    table:'Table',
+    poker:'Poker',
+    ball:'Ball',
+    virtual:'Virtual',
+    fun:'Fun' 
+    //
+    
+};
+export default strings;
