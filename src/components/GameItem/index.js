77c8737a3fc696a2c6957f0fd886c6e9f392f3e3
manhloi5501 <manhloi0505@gmail.com>
2022-05-01 16:53:59 +0700

    feat: add transition into card

--- src/components/GameItem/index.js
+++ src/components/GameItem/index.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import { Badge, Card, Image } from 'antd'
+import { Badge, Card, Image } from 'antd';
 
 import { useSelector } from 'react-redux';
-import styles from './styles.css'
-
+import styles from './styles.css';
 
 const GameItem = ({ game, showTag = false }) => {
   const { jackpots } = useSelector(state => state);
   const jackpotMatched = jackpots.find?.(jackpot => jackpot?.game === game?.id);
   const { amount } = jackpotMatched || {};
 
-  const renderGameImage = () =>
-
-    <Image
-      fallback='/undefined_img.jpg'
-      src={game?.image}
-      preview={false}
-    />
+  const renderGameImage = () => (
+    <Image fallback='/undefined_img.jpg' src={game?.image} preview={false} />
+  );
 
   return (
-      <Card
-        className={styles.itemCard}
-        bordered={false}
-      >
-        <Badge.Ribbon text="Hippies">
-          {renderGameImage()}
-        </Badge.Ribbon>
-
-      </Card>
-
+    <Card className={`${styles.itemCard} my-card`} bordered={false}>
+      {amount ? <h1 className='jackpot-amount'>{amount}</h1> : null}
+      <Badge.Ribbon text='Hippies'>{renderGameImage()}</Badge.Ribbon>
+    </Card>
   );
 };
 
